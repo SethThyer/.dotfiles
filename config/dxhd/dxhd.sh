0952183c7128188cbcb18298b2180bb6de36c101
SethThyer <seththyer23@gmail.com>
2021-09-16 19:15:35 +1000

    initial commit

---
+++ config/dxhd/dxhd.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+
+# super + s
+rofi -show drun -theme gruvbox-dark.rasi
+# super + print
+flameshot gui
+# super + e
+qutebrowser
+
+# XF86AudioPlay
+playerctl play-pause
+# XF86AudioNext
+playerctl next
+# XF86AudioPrev
+playerctl previous
